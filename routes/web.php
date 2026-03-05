ca6529c895854f6a13ac727d404699a942718439
Rhmn-n <ibnufatur1@gmail.com>
2026-03-05 19:07:44 +0700

    update koneksi database dan struktur folder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
-use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
-Route::get('/test-category', function () {
-    return Category::all();
+Route::get('/tes-koneksi', function () {
+    try {
+        // Mengambil daftar tabel langsung dari database localhost
+        $tables = DB::select('SHOW TABLES');
+        
+        return response()->json([
+            'status' => 'Koneksi Berhasil!',
+            'database' => DB::getDatabaseName(),
+            'daftar_tabel' => $tables
+        ]);
+    } catch (\Exception $e) {
+        return "Gagal konek ke phpMyAdmin: " . $e->getMessage();
+    }
 });
